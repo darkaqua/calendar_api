09c1f166610e5c0007ef03d87130336e605fb337
Pablo González <pagoru@gmail.com>
2017-03-23 02:10:09 +0100

    Fixed #8 + algunos ajustes

--- routes/info/version.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by pablo on 16/3/17.
- */
-module.exports = (app, express, request, response, next) => {
-    response.json({ version: global.package.version });
-};
--- routes/post/account/register.js
+++ routes/post/account/register.js
@@ -4,6 +4,7 @@
 const validators = require('../../../utils/validators');
 const crypt_utils = require('../../../utils/crypt-utils');
 const sql_source = require('../../../utils/sql-source');
+const uuid = require('uuid');
 
 module.exports = (app, express, request, response, next) => {
     const query = request.query;
@@ -33,16 +34,31 @@ const getRegisterResponse = (
     postal_code, //Puede estar vacio
     country) => {
     return new Promise((promise_result, promise_error) => {
-        if(name === undefined){
+        //Verificación del nombre
+        if(name === undefined || name.length <= 4){
             promise_result({ valid: false, message: `El campo del nombre no puede estar vacio` });
             return;
         }
+        if(name.length <= 4){
+            promise_result({ valid: false, message: `El nombre debe contener más carácteres` });
+            return;
+        }
+        //Verificación del nombre de usuario
         if(username === undefined){
             promise_result({ valid: false, message: `El campo del nombre de usuario no puede estar vacio` });
             return;
         }
+        if(username.length <= 4){
+            promise_result({ valid: false, message: `El nombre de usuario debe contener más carácteres` });
+            return;
+        }
+        //Verificación del correo electrónico
         if(email === undefined){
             promise_result({ valid: false, message: `El campo del correo electrónico no puede estar vacio` });
+            return;
+        }
+        if(!validators.verifyEmail(email)){
+            promise_result({ valid: false, message: `El correo electrónico no es valido` });
             return;
         }
         if(re_email === undefined){
@@ -53,8 +69,13 @@ const getRegisterResponse = (
             promise_result({ valid: false, message: `Los correos electrónicos no coinciden` });
             return;
         }
+        //Verificación de la contraseña
         if(password === undefined){
             promise_result({ valid: false, message: `El campo del contraseña no puede estar vacia` });
+            return;
+        }
+        if(password.length <= 8){
+            promise_result({ valid: false, message: `La contraseña debe contener más de 8 carácteres` });
             return;
         }
         if(re_password === undefined){
@@ -65,16 +86,68 @@ const getRegisterResponse = (
             promise_result({ valid: false, message: `Las contraseñas no coinciden` });
             return;
         }
+        //Verificación del pais
         if(country === undefined){
             promise_result({ valid: false, message: `El campo del pais no puede estar vacio` });
             return;
         }
-        //Username repetido
+        //country valido
+        const sql_conn = sql_source.connection();
+        const query = `SELECT count(*) AS count, id AS country_id FROM Country WHERE code=${sql_conn.escape(country)}`;
+        sql_conn.query(
+            query,
+            (sql_error, sql_results, sql_fields) => {
 
-        //email repetido
+                const sql_result = sql_results[0];
+                if(sql_result.count !== 1){
+                    promise_result({ valid: false, message: `El código de pais no es valido` });
+                    return;
+                }
+                //Username repetido
+                //email repetido
+                const query =
+                    `SELECT email, username FROM User 
+                    WHERE email=${sql_conn.escape(email)} 
+                    OR username=${sql_conn.escape(username)}`;
+                sql_conn.query(
+                    query,
+                    (sql_error_2, sql_results_2, sql_fields_2) => {
 
-        //
+                        if(sql_results_2.length !== 0){
+                            const sql_result_2 = sql_results_2[0];
 
-        promise_result({ valid: true }); //Volver al login
+                            if(sql_result_2.email === email){
+                                promise_result({ valid: false, message: `El correo electrónico ya esta registrado` });
+                                return;
+                            }
+
+                            promise_result({ valid: false, message: `El nombre de usuario ya esta registrado` });
+                            return;
+                        }
+
+                        const query =
+                            `INSERT INTO User(uuid, name, username, telephone, city, postal_code, email, hash, fk_country_id) 
+                            VALUES (
+                            ${sql_conn.escape(uuid.v4())},
+                            ${sql_conn.escape(name)},
+                            ${sql_conn.escape(username)},
+                            ${sql_conn.escape(telephone)},
+                            ${sql_conn.escape(city)},
+                            ${sql_conn.escape(postal_code)},
+                            ${sql_conn.escape(email)},
+                            ${sql_conn.escape(crypt_utils.bcrypt_encrypt(password))},
+                            ${sql_conn.escape(sql_result.country_id)}
+                            )`;
+                        sql_conn.query(
+                            query,
+                            (sql_error_3, sql_results_3, sql_fields_3) => {
+                                promise_result({ valid: true }); //Volver al login
+                            }
+                        );
+
+                    }
+                );
+            }
+        );
     });
 };
