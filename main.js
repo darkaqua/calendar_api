e1863ea2062a68684095683c5e6c625e5aac8f38
Pablo González <pagoru@gmail.com>
2017-03-25 19:35:10 +0100

    Fixed #25 + body-parser

--- main.js
+++ main.js
@@ -3,8 +3,11 @@
  */
 const fs = require('fs');
 const express = require('express');
+const body_parser = require("body-parser");
 
 const app = express();
+
+app.use(body_parser.json());
 
 require(`./functions`);
 
