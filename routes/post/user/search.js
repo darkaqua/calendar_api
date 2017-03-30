6b47a9c3a531ebed3555f4236f43e362c76a4a00
pagoru <pagoru@gmail.com>
2017-03-30 19:19:13 +0200

    Fixed #56 y cambios menores

---
+++ routes/post/user/search.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Pablo on 26/03/2017.
+ */
+const sql_source = require('../../../utils/sql-source');
+
+module.exports = (app, express, request, response, next) => {
+
+    global.functions.authRequest(request).then((auth) => {
+        if (!auth.valid) {
+            response.json(auth);
+            return;
+        }
+
+        const sql_conn = sql_source.connection();
+        const username = sql_conn.escape(request.body.search).replace(/'/g, '');
+        const query = `SELECT uuid, username FROM User WHERE username LIKE '%${username}%'`;
+        console.log(query);
+        sql_conn.query(query, (sql_error, sql_results, sql_fields) => {
+            response.json(sql_results.map((un) => un));
+        });
+
+    });
+
+};
