86ee4791bab4e3cbc7cbe3aa380c5095b5922bba
Pablo González <pagoru@gmail.com>
2017-05-10 15:12:57 +0200

    Fixing #106

--- metrics/m_registerd-users.js
+++ metrics/m_registerd-users.js
@@ -4,17 +4,19 @@
 const probe = require('pmx').probe();
 const sql_source = require('../utils/sql-source');
 
-let e = module.exports.metric = probe.metric({
-    name    : 'Registered users'
-});
+module.exports.init = () => {
+    module.exports.metric = probe.metric({
+        name    : 'Registered users'
+    });
 
-const sql_conn = sql_source.connection();
-const query = `SELECT COUNT(*) AS count FROM User`;
-sql_conn.query(query, (sql_error, sql_results, sql_fields) => {
-    if(sql_error) return;
-    if(!sql_results.length) return;
+    const sql_conn = sql_source.connection();
+    const query = `SELECT COUNT(*) AS count FROM User`;
+    sql_conn.query(query, (sql_error, sql_results, sql_fields) => {
+        if(sql_error) return;
+        if(!sql_results.length) return;
 
-    e.metric.set(sql_results[0].count);
+        module.exports.metric.set(sql_results[0].count);
 
-    sql_conn.close();
-});
+        sql_conn.end();
+    });
+};
