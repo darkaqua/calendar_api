2d8260e81507b22081aea10f17028b8449c2edbb
Pablo González <pagoru@gmail.com>
2017-05-13 18:01:49 +0200

    First step #85

---
+++ routes/put/company/group/date.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Pablo on 12/05/17.
+ */
+const sql_source = require('../../../../utils/sql-source');
+const validators = require('../../../../utils/validators');
+
+module.exports = (app, express, request, response, next) => {
+
+    global.functions.authRequest(request).then((auth) => {
+        if (!auth.valid) {
+            response.json(auth);
+            return;
+        }
+        const body = request.body;
+
+        //Validación de company_uuid
+        //Validación de group_id
+    });
+};
