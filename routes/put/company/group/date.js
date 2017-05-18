1f84cc481b75f7e6ce16d23fd3eb0d4f0a009f19
Pablo González <pagoru@gmail.com>
2017-05-18 03:30:14 +0200

    Fixed #85

--- routes/put/company/group/date.js
+++ routes/put/company/group/date.js
@@ -29,20 +29,105 @@ module.exports = (app, express, request, response, next) => {
             return;
         }
         //Comprobar que group_id es un numero
-        if(!isNaN(body.group_id)){
-            response.json({valid: false, message: `La id del grupo no puede estar vacia`});
+        if(isNaN(body.group_id)){
+            response.json({valid: false, message: `La id del grupo debe ser un numero valido`});
             return;
         }
         //Comprobar que el titulo no esta vacio
+        if(body.title === undefined){
+            response.json({valid: false, message: `El titulo no puede estar vacio`});
+            return;
+        }
         //Comprobar que el titulo tiene una longitud mayor a X
+        if(body.title.length < 4|| body.title.length >= 32){
+            response.json({valid: false, message: `El titulo debe contener mas de 4 caracteres y menos de 32`});
+            return;
+        }
         //Comprobar que la fecha no este vacia
+        if(body.datetime === undefined){
+            response.json({valid: false, message: `La fecha no puede estar vacia`});
+            return;
+        }
         //Comprobar que la fecha tenga un formato "dd/MM/yyyy hh:mm"
+        if(!global.functions.isValidDate(body.datetime)){
+            response.json({valid: false, message: `La fecha debe tener un formato dd/MM/yyyy hh:mm`});
+            return;
+        }
         //Comprobar que los minutos no estan vacios
+        if(body.long_minutes === undefined){
+            response.json({valid: false, message: `Los minutos de duración no pueden estar vacios`});
+            return;
+        }
         //Comprobar que los minutos sean numeros
+        if(isNaN(body.long_minutes)){
+            response.json({valid: false, message: `Los minutos de duración deben ser un número`});
+            return;
+        }
 
         //Comprobar que company_uuid existe
-        //Comprobar que group_id existe
-        //Comprobar que tieenes permisos de edición en la compañia o en el grupo
+        global.functions.isCompanyRegistered(body.company_uuid).then((isCompanyRegistered) => {
+            if (!isCompanyRegistered) {
+                response.json({valid: false, message: `La compañia no existe`});
+                return;
+            }
+            //Comprobar que group_id existe
+            global.functions.isGroupRegistered(body.company_uuid, body.group_id).then(isCompanyRegistered => {
+                if(!isCompanyRegistered){
+                    response.json({valid: false, message: `El grupo no existe`});
+                    return;
+                }
+                //Comprobar que tienes permisos de edición en la compañia o en el grupo
+                global.functions.hasUserPermissionToEditCompanyGroup(body.company_uuid, body.group_id, auth.user_uuid)
+                    .then((hasUserPermissionToEditCompanyGroup) => {
 
+                    global.functions.hasUserPermissionToEditCompany(body.company_uuid, auth.user_uuid)
+                        .then(hasUserPermissionToEditCompany => {
+
+                        if (!hasUserPermissionToEditCompanyGroup && !hasUserPermissionToEditCompany) {
+                            response.json({valid: false, message: `No tienes permisos en este grupo`});
+                            return;
+                        }
+
+                        //dd/MM/yyyy hh:mm
+                        const date1 = body.datetime.split(' ');
+                        const date2 = date1[0].split('/');
+                        const correctDateTime = `${date2[2]}-${date2[1]}-${date2[0]} ${date1[1]}`;
+
+                        const sql_conn = sql_source.connection();
+                        const query =
+                            `SET @last_id = (SELECT id 
+                                FROM CompanyGroupDate 
+                                WHERE fk_company_uuid=${sql_conn.escape(body.company_uuid)} 
+                                AND fk_group_id=${sql_conn.escape(body.group_id)} 
+                                ORDER BY id DESC 
+                                LIMIT 1);
+                            SET @next_id = (SELECT IFNULL(@last_id + 1, 0));
+                            INSERT INTO CompanyGroupDate(id, fk_group_id, fk_company_uuid, title, description, datetime, long_minutes)
+                            VALUES(
+                                @next_id,
+                                ${sql_conn.escape(body.group_id)},
+                                ${sql_conn.escape(body.company_uuid)},
+                                ${sql_conn.escape(body.title)},
+                                ${sql_conn.escape(body.description)},
+                                ${sql_conn.escape(correctDateTime)},
+                                ${sql_conn.escape(body.long_minutes)}
+                            );
+                            INSERT INTO UserLinkedCompanyGroupDate(fk_date_id, fk_group_id, fk_company_uuid, fk_user_uuid, can_edit)
+                            VALUES(
+                                @next_id,
+                                ${sql_conn.escape(body.group_id)},
+                                ${sql_conn.escape(body.company_uuid)},
+                                ${sql_conn.escape(auth.user_uuid)},
+                                '1'
+                            );
+                            SELECT @next_id AS date_id`;
+                        sql_conn.query(query, (sql_error, sql_results, sql_fields) => {
+                            response.json({ valid: true, group_id: sql_results[4][0].date_id});
+                            sql_conn.end();
+                        });
+                    });
+                });
+            });
+        });
     });
 };
